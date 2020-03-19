a08e16b58d40376bf617677d61082500eb48081e
HotSpor <hotspor00@gmail.com>
2020-03-19 19:20:07 +0200

    First commit

---
+++ src/main/java/asema/herokuapp/com/humiditysensor/HumiditysensorApplication.java
@@ -0,0 +1,13 @@
+package asema.herokuapp.com.humiditysensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumiditysensorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumiditysensorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/asema/herokuapp/com/humiditysensor/HumiditysensorApplicationTests.java
@@ -0,0 +1,13 @@
+package asema.herokuapp.com.humiditysensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumiditysensorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
